f8423931855114b2bca532d548c29d1df20b5f92
Hadi Hamadeh <ahamadeh@umich.edu>
2024-08-07 22:37:34 -0400

    Done

--- src/selectionsortclass/testSelectionSort.java
+++ src/selectionsortclass/testSelectionSort.java
@@ -48,38 +48,10 @@ class testSelectionSort {
 			//System.out.println(maxValue);
 		}
 		public void testMixes(){
-			int[] arr = new int[5];
-			arr[0] = -8;
-			arr[1] = 9;
-			arr[2] = 7;
-			arr[3] = 0;
-			arr[4] = 1;
-			SelectionSort temp1 = new SelectionSort();
-			int sorted[] = temp1.basicSelectionSort(arr);
-			int[] correct = {-8,0,1,7,9};
-			for (int i = 0; i < sorted.length; ++i) {
-				System.out.println(sorted[i]);
-			}
-		/** add tests to check for this unit test **/
-			assertArrayEquals(correct,sorted);
-			//System.out.println(maxValue);
+
 		}
 		public void testDuplicates() {
-			int[] arr = new int[5];
-			arr[0] = 1;
-			arr[1] = -9;
-			arr[2] = 7;
-			arr[3] = 0;
-			arr[4] = 1;
-			SelectionSort temp1 = new SelectionSort();
-			int sorted[] = temp1.basicSelectionSort(arr);
-			int[] correct = {-9,0,1,1,7};
-			for (int i = 0; i < sorted.length; ++i) {
-				System.out.println(sorted[i]);
-			}
-		/** add tests to check for this unit test **/
-			assertArrayEquals(correct,sorted);
-			//System.out.println(maxValue);
+
 		}
 		
 		}
