9811b736b5adc2a8c277c050d0d48ce5ff1989e6
Hadi Hamadeh <ahamadeh@umich.edu>
2024-08-07 22:01:03 -0400

    Initial

---
+++ src/selectionsortclass/testSelectionSort.java
@@ -0,0 +1,43 @@
+package selectionsortclass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testSelectionSort {
+
+	@Test
+	public void test() {
+		testPositive();
+		//testNegative();
+		//testMixes();
+		//testDuplicates();
+		}
+		public void testPositive(){
+			int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 12;
+		SelectionSort temp1 = new SelectionSort();
+		int sorted[] = temp1.basicSelectionSort(arr);
+		int[] correct = {7,8,9,10,12};
+		for (int i = 0; i < sorted.length; ++i) {
+			System.out.println(sorted[i]);
+		}
+	/** add tests to check for this unit test **/
+		assertArrayEquals(correct,sorted);
+		//System.out.println(maxValue);
+		}
+		public void testNegative(){
+
+		}
+		public void testMixes(){
+			
+		}
+		public void testDuplicates() {
+			
+		}
+		
+		}
