dacfa79a8513a8b23323c71be5dd8347b608406a
Hadi Hamadeh <ahamadeh@umich.edu>
2024-08-07 23:14:39 -0400

    PosNeg

--- src/selectionsortclass/testSelectionSort.java
+++ src/selectionsortclass/testSelectionSort.java
@@ -26,6 +26,9 @@ class testSelectionSort {
 			for (int i = 0; i < sorted.length; ++i) {
 				System.out.println(sorted[i]);
 			}
+			/** add tests to check for this unit test **/
+			assertArrayEquals(correct,sorted);
+			//System.out.println(maxValue);
 		}
 		public void testNegative(){
 			int[] arr = new int[5];
@@ -40,6 +43,9 @@ class testSelectionSort {
 			for (int i = 0; i < sorted.length; ++i) {
 				System.out.println(sorted[i]);
 			}
+			/** add tests to check for this unit test **/
+			assertArrayEquals(correct,sorted);
+			//System.out.println(maxValue);
 		}
 		public void testMixes(){
 
