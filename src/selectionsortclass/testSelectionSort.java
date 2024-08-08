c978a11ff2e1a58671bd82e0b5a0a3303a640b9f
Hadi Hamadeh <ahamadeh@umich.edu>
2024-08-07 22:16:18 -0400

    Done Branch

--- src/selectionsortclass/testSelectionSort.java
+++ src/selectionsortclass/testSelectionSort.java
@@ -8,10 +8,10 @@ class testSelectionSort {
 
 	@Test
 	public void test() {
-		testPositive();
+		//testPositive();
 		//testNegative();
 		//testMixes();
-		//testDuplicates();
+		testDuplicates();
 		}
 		public void testPositive(){
 			int[] arr = new int[5];
@@ -31,13 +31,55 @@ class testSelectionSort {
 		//System.out.println(maxValue);
 		}
 		public void testNegative(){
-
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = -10;
+			arr[4] = -2;
+			SelectionSort temp1 = new SelectionSort();
+			int sorted[] = temp1.basicSelectionSort(arr);
+			int[] correct = {-10,-9,-8,-7,-2};
+			for (int i = 0; i < sorted.length; ++i) {
+				System.out.println(sorted[i]);
+			}
+		/** add tests to check for this unit test **/
+			assertArrayEquals(correct,sorted);
+			//System.out.println(maxValue);
 		}
 		public void testMixes(){
-			
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = 9;
+			arr[2] = 7;
+			arr[3] = 0;
+			arr[4] = 1;
+			SelectionSort temp1 = new SelectionSort();
+			int sorted[] = temp1.basicSelectionSort(arr);
+			int[] correct = {-8,0,1,7,9};
+			for (int i = 0; i < sorted.length; ++i) {
+				System.out.println(sorted[i]);
+			}
+		/** add tests to check for this unit test **/
+			assertArrayEquals(correct,sorted);
+			//System.out.println(maxValue);
 		}
 		public void testDuplicates() {
-			
+			int[] arr = new int[5];
+			arr[0] = 1;
+			arr[1] = -9;
+			arr[2] = 7;
+			arr[3] = 0;
+			arr[4] = 1;
+			SelectionSort temp1 = new SelectionSort();
+			int sorted[] = temp1.basicSelectionSort(arr);
+			int[] correct = {-9,0,1,1,7};
+			for (int i = 0; i < sorted.length; ++i) {
+				System.out.println(sorted[i]);
+			}
+		/** add tests to check for this unit test **/
+			assertArrayEquals(correct,sorted);
+			//System.out.println(maxValue);
 		}
 		
 		}
