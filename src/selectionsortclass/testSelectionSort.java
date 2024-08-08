a4a963a7ff4403b97f5cdc112a9a5e987cffaa84
Hadi Hamadeh <ahamadeh@umich.edu>
2024-08-07 23:02:48 -0400

    Fixed Done

--- src/selectionsortclass/testSelectionSort.java
+++ src/selectionsortclass/testSelectionSort.java
@@ -8,40 +8,43 @@ class testSelectionSort {
 
 	@Test
 	public void test() {
-		//testPositive();
-		//testNegative();
-		//testMixes();
+		testPositive();
+		testNegative();
+		testMixes();
 		testDuplicate();
 		}
 		public void testPositive(){
-
+			int[] arr = new int[5];
+			arr[0] = 8;
+			arr[1] = 9;
+			arr[2] = 7;
+			arr[3] = 10;
+			arr[4] = 12;
+			SelectionSort temp1 = new SelectionSort();
+			int sorted[] = temp1.basicSelectionSort(arr);
+			int[] correct = {7,8,9,10,12};
+			for (int i = 0; i < sorted.length; ++i) {
+				System.out.println(sorted[i]);
+			}
 		}
 		public void testNegative(){
-
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = -10;
+			arr[4] = -2;
+			SelectionSort temp1 = new SelectionSort();
+			int sorted[] = temp1.basicSelectionSort(arr);
+			int[] correct = {-10,-9,-8,-7,-2};
+			for (int i = 0; i < sorted.length; ++i) {
+				System.out.println(sorted[i]);
+			}
 		}
 		public void testMixes(){
 
 		}
-<<<<<<< HEAD
-		public void testDuplicates() {
-
-=======
 		public void testDuplicate() {
-			int[] arr = new int[5];
-			arr[0] = 1;
-			arr[1] = -9;
-			arr[2] = 7;
-			arr[3] = 0;
-			arr[4] = 1;
-			SelectionSort temp1 = new SelectionSort();
-			int sorted[] = temp1.basicSelectionSort(arr);
-			int[] correct = {-9,0,1,1,7};
-			for (int i = 0; i < sorted.length; ++i) {
-				System.out.println(sorted[i]);
-			}
-		/** add tests to check for this unit test **/
-			assertArrayEquals(correct,sorted);
-			//System.out.println(maxValue);
->>>>>>> fb1d324947a168c7ab92ec16c2159f91f10a62e7
+
 		}
 }
